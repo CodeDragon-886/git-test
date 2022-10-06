0b210727502221006b570a19c0895882c26823e8
龙千里 <766754319@qq.com>
2022-10-06 08:33:52 +0000

    update src/main/java/com/atguigu/git/GitTest.java.
    Signed-off-by: 龙千里 <766754319@qq.com>

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("master test");
         System.out.println("哈哈哈哈哈push成了1111");
+         System.out.println("那小子真帅-gitee搞起来");
     }
 }
